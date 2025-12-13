2b051d12d99a6d91cffcb9a7a0276f0c2bfa44b7
ThVariableYT <jashg211007@gmail.com>
2025-12-13 11:16:30 +0530

    Fix and improve single inheritance example in Practical 5A
    Corrected typos in comments and method names, updated attribute names to use underscores for encapsulation, fixed object instantiation for Rectangle, and ensured area calculation uses the correct attributes.

--- OOP/Practical 5A.py
+++ OOP/Practical 5A.py
@@ -1,17 +1,17 @@
 #5.a Single inheritance
-#Befine base class Shape
+#Define base class Shape
 class Shape:
     #Function to initialize the data members def get Length Breadth(self):
     def _get_Length_Breadth(self):
-        self.length=int(input("Enter length:"))
-        self.breadth=int(input("Enter breadth:"))
+        self._length=int(input("Enter length:"))
+        self._breadth=int(input("Enter breadth:"))
 #Defining derived class Rectangle'
 class Rectangle(Shape):
-    #Fuunction to calculate area of rectangle  
+    #Function to calculate area of rectangle  
     def calculate_area(self):
-        print("area of Rectangle is", self.length* self.breadth)
+        print("area of Rectangle is", self._length* self._breadth)
 #Creating object
-obj=Rectangle
+obj=Rectangle()
 #Calling function to get input
 obj._get_Length_Breadth()
 #Calling function to get the area of rectangle
