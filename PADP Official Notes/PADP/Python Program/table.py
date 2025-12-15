9aa7cffacd7e087a5dffede148734909ec96f389
Jash Gandhi <jashg211007@gmail.com>
2025-12-15 19:04:10 +0530

    Fix indentation in table.py function body

--- PADP Official Notes/PADP/Python Program/table.py
+++ PADP Official Notes/PADP/Python Program/table.py
@@ -7,12 +7,12 @@ def tablefinder():
         limit = 0
         m = 0
     
-        enter = int(enter)
-        limit = enter*10+1
-        m=1
-    for i in range(enter,limit,enter):
-            tk.Label(win,enter , " × " ,m, " = " , i)
-            m += 1
-    enter = tk.Entry(win,bd=10)
-    enter.place(x=0,y=50)
-    tk.Button(win,text="Calculate",command=tablefinder).pack()
+                enter = int(enter)
+                limit = enter*10+1
+                m=1
+            for i in range(enter,limit,enter):
+                            tk.Label(win,enter , " × " ,m, " = " , i)
+                            m += 1
+            enter = tk.Entry(win,bd=10)
+            enter.place(x=0,y=50)
+            tk.Button(win,text="Calculate",command=tablefinder).pack()
