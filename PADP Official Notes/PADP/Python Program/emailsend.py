cf6f6cfb57fe7c9d452077a1eb8da6cc5759564e
Jash Gandhi <jashg211007@gmail.com>
2025-12-15 18:24:14 +0530

    Fix unterminated string literal error in emailsend.py

--- PADP Official Notes/PADP/Python Program/emailsend.py
+++ PADP Official Notes/PADP/Python Program/emailsend.py
@@ -2,7 +2,7 @@ import smtplib,ssl
 
 #content="Subject: Hi user \n hello,sending mail from python code"
 
-email_to=['receiver's email id']
+email_to=["receiver's email id"]
 content=f'''Subject: A plain text email
 To: {','.join(email_to)}
 This is a new test email sent from python.'''
