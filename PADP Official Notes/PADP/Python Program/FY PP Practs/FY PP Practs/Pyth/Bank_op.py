80523a65b09ba1397ba52f2728f0d7187431bdbf
Jash Gandhi <jashg211007@gmail.com>
2025-12-15 19:02:12 +0530

    Fix class method signatures and code structure in Bank_op.py

--- PADP Official Notes/PADP/Python Program/FY PP Practs/FY PP Practs/Pyth/Bank_op.py
+++ PADP Official Notes/PADP/Python Program/FY PP Practs/FY PP Practs/Pyth/Bank_op.py
@@ -3,7 +3,7 @@ class Bank:
   acctno=0
   initial_bal=0.0
   amt=0.0
-  def accept():
+  def accept(self):
     print("enter name,acctno")
     name=input()
     acctno=int(input())
@@ -12,7 +12,7 @@ class Bank:
     print(name)
     print(acctno)
     print(Bank.initial_bal)
-  def deposit():
+  def deposit(self):
      print("enter amount to be deposited")
      amt=int(input())
      self.initial_bal=self.initial_bal+self.amt
@@ -22,11 +22,11 @@ class Bank:
      self.amt=int(input())
      self.initial_bal=self.initial_bal-self.amt
      print("updated balance is",Bank.initial_bal)
-  b1=Bank()
-  b1.accept()
-  b1.deposit()
-  b1.withdraw()
-  
+b1=Bank()
+b1.accept()
+b1.deposit()
+b1.withdraw()
+
 
      
                   
