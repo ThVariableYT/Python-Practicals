27a9f0a7a9c822aa8627ad012b6d38e3b06742ed
Jash Gandhi <jashg211007@gmail.com>
2025-12-15 18:27:07 +0530

    Fix variable scope and indentation errors in table.py

--- PADP Official Notes/PADP/Python Program/table.py
+++ PADP Official Notes/PADP/Python Program/table.py
@@ -3,12 +3,16 @@ import tkinter.font as font
 win = tk.Tk()
 win.minsize(300,200)
 def tablefinder():
-    enter = int(enter)
-    limit = enter*10+1
-    m=1
-for i in range(enter,limit,enter):
-    tk.Label(win,enter , " × " ,m, " = " , i)
-    m += 1
-enter = tk.Entry(win,bd=10)
-enter.place(x=0,y=50)
-tk.Button(win,text="Calculate",command=tablefinder).pack()
+        enter = 0
+        limit = 0
+        m = 0
+    
+        enter = int(enter)
+        limit = enter*10+1
+        m=1
+    for i in range(enter,limit,enter):
+            tk.Label(win,enter , " × " ,m, " = " , i)
+            m += 1
+    enter = tk.Entry(win,bd=10)
+    enter.place(x=0,y=50)
+    tk.Button(win,text="Calculate",command=tablefinder).pack()
