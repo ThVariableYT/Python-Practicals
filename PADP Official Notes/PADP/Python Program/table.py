692ebd2ed19d9d2404fa862a2134208078a65287
Jash Gandhi <jashg211007@gmail.com>
2025-12-15 19:09:11 +0530

    Fix line 12 indentation in table.py

--- PADP Official Notes/PADP/Python Program/table.py
+++ PADP Official Notes/PADP/Python Program/table.py
@@ -9,7 +9,7 @@ def tablefinder():
     
 #                 # enter = int(enter)
 #                 limit = enter*10+1
-                m=1
+                    m=1
             for i in range(enter,limit,enter):
                             tk.Label(win,enter , " × " ,m, " = " , i)
                             m += 1
