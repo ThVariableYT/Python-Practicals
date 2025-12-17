3402f25151101530355d2f7fb1e7b6678ae5b0b9
Jash Gandhi <jashg211007@gmail.com>
2025-12-17 20:24:27 +0530

    Fix indentation and formatting in table.py

--- PADP Official Notes/PADP/Python Program/table.py
+++ PADP Official Notes/PADP/Python Program/table.py
@@ -9,9 +9,10 @@ def tablefinder():
     
 #                 # enter = int(enter)
 #                 limit = enter*10+1
-m=1            for i in range(enter,limit,enter):
-                            tk.Label(win,enter , " × " ,m, " = " , i)
-                            m += 1
+m=1
+for i in range(enter,limit,enter):
+                tk.Label(win,enter , " × " ,m, " = " , i)
+                                    m += 1
             enter = tk.Entry(win,bd=10)
             enter.place(x=0,y=50)
             tk.Button(win,text="Calculate",command=tablefinder).pack()
